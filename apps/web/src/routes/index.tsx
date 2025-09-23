dbad0253a0f29eb86ac3803bfc76f0e47c1fb9a4
Franklin Esquivel <franklin.esquivel@outlook.com>
2025-09-23 10:51:16 -0600

    chore(web): Removes unused vars.

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { toast } from 'sonner';
 import { BookingsTable } from '@web/components/bookings/BookingsTable.tsx';
 import { CreateBookingDialog } from '@web/components/bookings/components/CreateBookingDialog.tsx';
-import { Button } from '@web/components/ui/button.tsx';
 import { UserNavBar } from '@web/components/users/UserNavBar.tsx';
 import { useGetUserBookings } from '@web/hooks/useGetUserBookings.ts';
 import { authStore } from '@web/stores/auth.store.ts';
